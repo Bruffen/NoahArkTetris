c2f2219fc7114d1a17aaf9b367c6a968de2a9e1d
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2019-05-24 22:46:20 +0100

    Added Tetris UI

---
+++ Assets/Scripts/Tetris/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item")]
+public class Item : ScriptableObject {
+    [HideInInspector]
+    public bool[] shapeArray;
+    public uint height;
+    public uint width;
+}
