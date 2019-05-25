b5b18c01b182528046c20dc9676f5ae571102dab
Bruffen <brunofreitas5.25@hotmail.com>
2019-05-25 15:47:15 +0100

    AI and Ship

---
+++ Assets/Scripts/Ship/ShipManagerWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    public class ShipManagerWrapper : MonoBehaviour
+    {
+        public Transform[] LadderPositions;
+        public int LadderCooldown = 500;
+        public Vector2 Size;
+        public int FloorsNumber;
+
+        public Ladder[] ladders;
+
+        void Start()
+        {
+            ladders = new Ladder[LadderPositions.Length];
+            for (int i = 0; i < ladders.Length; i++)
+            {
+                ladders[i].Position = LadderPositions[i].localPosition;
+                ladders[i].InUse = false;
+            }
+
+            ShipManager.Ladders = ladders;
+            ShipManager.Size = Size;
+            ShipManager.LadderCooldownTime = LadderCooldown;
+            ShipManager.FloorsNumber = FloorsNumber;
+            ShipManager.FloorHeight = Size.y / (float)FloorsNumber;
+            if (FloorsNumber == 0) Debug.LogError("Set number of floors!");
+        }
+    }
+}
